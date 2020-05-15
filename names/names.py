065479cad61910df19c37e60d26403cfe23760bb
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-15 08:31:26 -0700

    complete names.py using a dictionary

--- names/names.py
+++ names/names.py
@@ -13,10 +13,10 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -26,3 +26,27 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+print("\nStretch goal times:")
+
+start_time = time.time()
+
+# dictionary lookup is constant time, so store names in a dictionary
+names_dictionary = dict()
+
+# store discovered duplicates in an array
+stretch_duplicates = []
+
+# add all names in the first list to the dictionary
+for name_1 in names_1:
+    names_dictionary[name_1] = 1
+
+# check if each name in the second list is in the dictionary
+for name_2 in names_2:
+    if name_2 in names_dictionary:
+        stretch_duplicates.append(name_2)
+
+# print time and duplicates
+end_time = time.time()
+print (f"{len(stretch_duplicates)} stretch_duplicates:\n\n{', '.join(stretch_duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
