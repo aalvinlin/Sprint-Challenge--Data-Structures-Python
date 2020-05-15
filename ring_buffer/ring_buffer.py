2690d3bf401fd2ecbd39fb81d5b09567cb9dc069
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-15 14:01:41 -0700

    implement print_start pointer, array representation, and class inheritance

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -15,6 +15,9 @@ class CircularLinkedList:
         # store a pointer to access one of the nodes in the linked list
         self.entry = newest
 
+        # even though rings don't have a start, maintain a start pointer for a consistent printing format
+        self.print_start = newest
+
         # initialize all nodes to empty
         for i in range(length - 1):
             newest.next = Node()
@@ -25,11 +28,11 @@ class CircularLinkedList:
 
     def __str__(self):
 
-        current_node = self.oldest
+        current_node = self.print_start
 
         string_representation = str(current_node.value)
 
-        while current_node.next is not self.oldest:
+        while current_node.next is not self.print_start:
 
             current_node = current_node.next
 
@@ -37,12 +40,39 @@ class CircularLinkedList:
             if current_node.value:
                 string_representation += " -> " + str(current_node.value)
 
+        # repeat the starting node to show circularity
+        string_representation += " -> " + str(self.print_start)
+
         return string_representation
 
-class RingBuffer:
+    def get_array_representation(self):
+
+        current_node = self.print_start
+
+        array_representation = []
+
+        # add first element if it is not None
+        if current_node.value:
+            array_representation.append(str(current_node.value))
+
+        while current_node.next is not self.print_start:
+
+            current_node = current_node.next
+
+            # do not display empty nodes
+            if current_node.value:
+
+                array_representation.append(str(current_node.value))
+
+        return array_representation
+
+class RingBufferAsCircularLinkedList:
     def __init__(self, capacity):
         self.capacity = capacity        
         self.storage = CircularLinkedList(5)
+
+    def __str__(self):
+        return str(self.storage)
         
     def append(self, item):
         # store item in next available spot
@@ -52,8 +82,37 @@ class RingBuffer:
         self.storage.entry = self.storage.entry.next
 
     def get(self):
-        return
+        return self.storage.get_array_representation()
 
-ring = CircularLinkedList(5)
-print(ring)
+class RingBuffer(RingBufferAsCircularLinkedList):
+    def __init__(self, capacity):
+        super().__init__(capacity)
+
+# ring = RingBuffer(5)
+# ring.append('a')
+# ring.append('b')
+# ring.append('c')
+# ring.append('d')
+# ring.append('e')
+# ring.append('f')
+# print(ring.get())
+
+# ring.append('g')
+# print(ring.get())
+
+# ring.append('h')
+# print(ring.get())
+
+# ring.append('i')
+# print(ring.get())
+
+# ring.append('j')
+# print(ring.get())
+
+# ring.append('k')
+# print(ring.get())
+
+# ring.append('l')
+# print(ring.get())
+
 
